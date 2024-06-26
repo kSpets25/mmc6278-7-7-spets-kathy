e83652273c02189fd3cec2d518391009f0fe3dfe
kSpets25 <142757766+kSpets25@users.noreply.github.com>
2024-06-26 13:27:17 -0500

    added required files to assignment and updated .gitignore file

---
+++ schema/schema.sql
@@ -0,0 +1,38 @@
+USE music_shop_db;
+
+CREATE TABLE inventory (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  image VARCHAR(100) NOT NULL,
+  description TEXT,
+  price FLOAT(10,2) NOT NULL DEFAULT 0,
+  quantity INT NOT NULL DEFAULT 0
+);
+
+-- Create the users table with
+-- id that's primary key integer auto incrementing
+-- username that's a 100 char varchar and unique
+-- password that's a 200 char varchar
+
+CREATE TABLE users (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  username VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(200) NOT NULL
+
+)
+
+CREATE TABLE cart (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  inventory_id INT NOT NULL,
+-- add a user_id column that's an int
+  user_id INT NOT NULL,
+  is_done BOULEAN DEFAULT false,
+  quantity INT NOT NULL DEFAULT 1,
+  FOREIGN KEY (inventory_id),
+    REFERENCES inventory (id),
+    ON DELETE CASCADE,
+-- add a foreign key constraint to user_id just like inventory_id
+  FOREIGN KEY (user_id)
+    REFERENCES user (id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
